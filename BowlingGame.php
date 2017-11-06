8d29f968dcc7194220934d6d90280d4b76728bd0
David Walters <whatbirdisthat@gmail.com>
2017-11-06 13:35:51 +1100

    refactor for more meaningful method names

--- BowlingGame.php
+++ BowlingGame.php
@@ -10,13 +10,21 @@ class BowlingGame {
     $this->bowlCount++;
   }
 
+  function isStrike($i) {
+    return ($i < count($this->bowls)-3) && $this->bowls[$i] == 10;
+  }
+
+  function isSpare($i) {
+    return $i > 0 && $this->bowls[$i] + $this->bowls[$i-1] == 10;
+  }
+
   function score() {
     $theScore = 0;
 
     for ($i = 0; $i < count($this->bowls); $i++) {
-      if (($i < count($this->bowls)-3) && $this->bowls[$i] == 10) {
+      if ($this->isStrike($i)) {
         $theScore += $this->bowls[$i] + $this->bowls[$i+1] + $this->bowls[$i+2];
-      } else if ($i > 0 && $this->bowls[$i] + $this->bowls[$i-1] == 10) {
+      } else if ($this->isSpare($i)) {
         $theScore += $this->bowls[$i] + $this->bowls[$i+1];
       } else {
         $theScore += $this->bowls[$i];
